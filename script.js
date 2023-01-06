8ed0d7f4d41f9591ad819f904d09c9f88c9fd198
Krishna Kumar <86869782+GitHubKrishnaKumar@users.noreply.github.com>
2023-01-06 15:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let text = document.getElementById("evaluatedText");
+let count = document.getElementById("letterCount");
+
+function charCount() {
+	if(text == ""){
+		count.innerHTML = 0;
+	}
+	else{
+		count.innerHTML = text.value.length;
+	}
+}
+text.oninput = charCount;
