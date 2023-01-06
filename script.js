709a042803a58dce1ba1c438444afaf248ce38b7
Krishna Kumar <86869782+GitHubKrishnaKumar@users.noreply.github.com>
2023-01-06 15:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ let text = document.getElementById("evaluatedText");
 let count = document.getElementById("letterCount");
 
 function charCount() {
-	if(text.value.length == 0){
-		count.innerHTML = 0;
-	}
-	else{
-		count.innerHTML = text.value.length;
-	}
+	
+	count.innerHTML = text.value.length;
 }
 text.oninput = charCount;
